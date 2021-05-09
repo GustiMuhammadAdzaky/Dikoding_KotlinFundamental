28307d6c3afaf3b8f92861c0852740bf3365492c
GustiMuhammadAdzaky <xxzakixx001@gmail.com>
2021-05-09 22:10:04 +0700

    Lanjutan

--- src/String.kt
+++ src/String.kt
@@ -1,17 +1,44 @@
 fun main() {
 
-    println("-----------------Indexing String------------------")
+    println("-----------------Indexing String & For String------------------")
 
     val text = "kotlin"
     val firstChar = text[0]
     println("$text $firstChar")
 
-
-    println("-------------------For String------------------")
-
-
     val text1  = "Kotlin"
     for (char in text1){
-        print("$char ")
+        println("$char ")
     }
+
+    println("-----------------Escaped String-----------------")
+    //\t: menambah tab ke dalam teks.
+    //
+    //\n: membuat baris baru di dalam teks.
+    //
+    //\’: menambah karakter single quote kedalam teks.
+    //
+    //\”: menambah karakter double quote kedalam teks.
+    //
+    //\\: menambah karakter backslash kedalam teks.
+
+    val statement = "Kotlin is \"Awesome!\""
+    println(statement)
+
+//    Unicode
+    val name = "Unicode test: \u00A9"
+    println(name)
+
+    println("-------------Raw String-------------")
+
+        val line = """
+        Line 1
+        Line 2
+        Line 3
+        Line 4
+    """.trimIndent()
+
+        println(line)
+
+
 }
