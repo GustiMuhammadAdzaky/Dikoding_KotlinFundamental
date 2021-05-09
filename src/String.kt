33f232b81ff065e70e9d8c30fdadc295c743cdf6
GustiMuhammadAdzaky <xxzakixx001@gmail.com>
2021-05-09 15:37:18 +0700

    Lanjutan

---
+++ src/String.kt
@@ -0,0 +1,17 @@
+fun main() {
+
+    println("-----------------Indexing String------------------")
+
+    val text = "kotlin"
+    val firstChar = text[0]
+    println("$text $firstChar")
+
+
+    println("-------------------For String------------------")
+
+
+    val text1  = "Kotlin"
+    for (char in text1){
+        print("$char ")
+    }
+}
